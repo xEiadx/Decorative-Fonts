7780fe41124ea5a51d99550e754b59521f020f56
xEiadx <128938790+xEiadx@users.noreply.github.com>
2023-03-26 00:57:19 +0200

    Update Italic.js

--- Italic.js
+++ Italic.js
@@ -59,6 +59,8 @@ function Italic(userName) {
     let char = userName[i];
     if (decorMap[char]) {
       decorname += decorMap[char];
+    } else {
+      decorname += char;
     }
   }
   return decorname;
