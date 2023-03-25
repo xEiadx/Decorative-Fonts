a3700936ebd4c9ddcd073d7455caf3e20e152e32
xEiadx <128938790+xEiadx@users.noreply.github.com>
2023-03-26 00:56:20 +0200

    Update bold.js

--- bold.js
+++ bold.js
@@ -59,6 +59,8 @@ function bold(userName) {
     let char = userName[i];
     if (decorMap[char]) {
       decorname += decorMap[char];
+    } else {
+      decorname += char;
     }
   }
   return decorname;
